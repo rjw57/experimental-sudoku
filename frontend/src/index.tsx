999974ffefbea9e5f7621f1b59786eb1bf2c7958
Rich Wareham <rich@richwareham.com>
2021-01-02 12:29:27 +0000

    wire in emulators

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebaseAppConfig from './firebaseAppConfig';
+
+// Initialise firebase
+firebase.initializeApp(firebaseAppConfig);
+firebase.auth().useDeviceLanguage();
+
+if(process.env.NODE_ENV === 'development') {
+  console.log('Running in development, using emulators');
+  firebase.auth().useEmulator('http://localhost:9099/');
+  firebase.firestore().useEmulator('localhost', 8080);
+}
 
 ReactDOM.render(
   <React.StrictMode>
