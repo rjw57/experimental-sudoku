859e688afd570bd712955dfed08e06655f78d5cd
Rich Wareham <rich@richwareham.com>
2021-01-03 14:32:08 +0000

    implement routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import { SolvePage } from './pages';
 
 export const App = () => (
-  <SolvePage />
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Link to="/solve/foo">example</Link>
+      </Route>
+      <Route exact path="/solve/:puzzleId">
+        <SolvePage />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
